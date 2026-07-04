302466e77ce7e819823d740169fb4756b4ff1480
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-07-03 20:00:00 -0500

    feat: initialize Spring Boot 3.2 project structure

---
+++ src/test/java/edu/eci/arsw/raceflow/metrics/MetricsApplicationTests.java
@@ -0,0 +1,10 @@
+package edu.eci.arsw.raceflow.metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsApplicationTests {
+    @Test
+    void contextLoads() {}
+}
